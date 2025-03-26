d91892e95af1ee5789e42d241ca1c5fd925a36fe
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-03-26 12:39:38 +0200

    removed backend ping at load

--- src/App.jsx
+++ src/App.jsx
@@ -12,61 +12,61 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const VITE_API_BASE_URL = "https://backend-ecom-gbzk.onrender.com";
-const VITE_API_BASE_URL_PADLET = "https://friends-2v7s.onrender.com";
+// const VITE_API_BASE_URL = "https://backend-ecom-gbzk.onrender.com";
+// const VITE_API_BASE_URL_PADLET = "https://friends-2v7s.onrender.com";
 
 function App() {
   const workRef = useRef(null);
   const contactRef = useRef(null);
   const aboutRef = useRef(null);
 
-  useEffect(() => {
-    const pingBackend = async (retries = 3, delay = 5000) => {
-      for (let attempt = 1; attempt <= retries; attempt++) {
-        try {
-          const res = await fetch(`${VITE_API_BASE_URL}/api/ping`);
-          if (res.ok) {
-            console.log("E-Commerce Store Backend is awake");
-            return;
-          }
-        } catch (error) {
-          console.warn(`Ping attempt ${attempt} failed.`);
-          if (attempt < retries) {
-            await new Promise((resolve) => setTimeout(resolve, delay));
-          }
-        }
-      }
+  // useEffect(() => {
+  //   const pingBackend = async (retries = 3, delay = 5000) => {
+  //     for (let attempt = 1; attempt <= retries; attempt++) {
+  //       try {
+  //         const res = await fetch(`${VITE_API_BASE_URL}/api/ping`);
+  //         if (res.ok) {
+  //           console.log("E-Commerce Store Backend is awake");
+  //           return;
+  //         }
+  //       } catch (error) {
+  //         console.warn(`Ping attempt ${attempt} failed.`);
+  //         if (attempt < retries) {
+  //           await new Promise((resolve) => setTimeout(resolve, delay));
+  //         }
+  //       }
+  //     }
 
-      console.error(
-        "Failed to wake E-Commerce Store backend after multiple attempts."
-      );
-    };
+  //     console.error(
+  //       "Failed to wake E-Commerce Store backend after multiple attempts."
+  //     );
+  //   };
 
-    pingBackend();
-  }, []);
+  //   pingBackend();
+  // }, []);
 
-  useEffect(() => {
-    const pingBackend = async (retries = 3, delay = 5000) => {
-      for (let attempt = 1; attempt <= retries; attempt++) {
-        try {
-          const res = await fetch(`${VITE_API_BASE_URL_PADLET}/api/ping`);
-          if (res.ok) {
-            console.log("Todo App Backend is awake");
-            return;
-          }
-        } catch (error) {
-          console.warn(`Ping attempt ${attempt} failed.`);
-          if (attempt < retries) {
-            await new Promise((resolve) => setTimeout(resolve, delay));
-          }
-        }
-      }
+  // useEffect(() => {
+  //   const pingBackend = async (retries = 3, delay = 5000) => {
+  //     for (let attempt = 1; attempt <= retries; attempt++) {
+  //       try {
+  //         const res = await fetch(`${VITE_API_BASE_URL_PADLET}/api/ping`);
+  //         if (res.ok) {
+  //           console.log("Todo App Backend is awake");
+  //           return;
+  //         }
+  //       } catch (error) {
+  //         console.warn(`Ping attempt ${attempt} failed.`);
+  //         if (attempt < retries) {
+  //           await new Promise((resolve) => setTimeout(resolve, delay));
+  //         }
+  //       }
+  //     }
 
-      console.error("Failed to wake Todo App backend after multiple attempts.");
-    };
+  //     console.error("Failed to wake Todo App backend after multiple attempts.");
+  //   };
 
-    pingBackend();
-  }, []);
+  //   pingBackend();
+  // }, []);
 
   useEffect(() => {
     const lenis = new Lenis({
