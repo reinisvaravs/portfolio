47708f0b98d28e27ad8db6d10a86cb0bc396a275
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-07-09 07:07:15 +0300

    fix

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 0.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
     });
