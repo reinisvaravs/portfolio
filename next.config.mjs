930a8c330ff30b857edaa67b1c9192c7af9f148f
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-07-18 18:46:10 +0300

    added backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://web-chatbot-nbg8.onrender.com/api/:path*", // Proxy to deployed backend
+      },
+    ];
+  },
   images: {
     domains: ["reinisvaravs.com"],
     formats: ["image/avif", "image/webp"],
