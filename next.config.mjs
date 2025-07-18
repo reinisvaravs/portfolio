e93b4f5ba6829fcb85676cf45359ad8856b4a1d5
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-07-18 19:07:40 +0300

    removed backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://web-chatbot-nbg8.onrender.com/api/:path*", // Proxy to deployed backend
-      },
-    ];
-  },
   images: {
     domains: ["reinisvaravs.com"],
     formats: ["image/avif", "image/webp"],
